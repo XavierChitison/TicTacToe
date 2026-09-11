0a1acd905a9a3f640a276bf6da45f02e289ff3cf
Xavier Chitison <chitisonx@gmail.com>
2026-09-11 11:59:27 -0400

    map out project

---
+++ TicTacToe.py
@@ -0,0 +1,40 @@
+"""
+Program: Tic-Tac-Toe Game
+Author: Xavier Chitison
+Purpose: A two-player Tic-Tac-Toe game that allows players to enter
+         their names, take turns placing X and O, detects winners and
+         ties, keeps score, and allows multiple rounds to be played.
+Resources: Python Crash Course, Chapters 1-7
+Date: September 11, 2026
+"""
+
+"""
+Output example:
+=================================
+        TIC-TAC-TOE
+=================================
+
+Player 1, enter your name: Xavier
+Player 2, enter your name: Mason
+
+Xavier = X
+Mason = O
+
+     1 | 2 | 3
+    ---+---+---
+     4 | 5 | 6
+    ---+---+---
+     7 | 8 | 9
+
+Xavier (X), choose a position: 5
+
+     1 | 2 | 3
+    ---+---+---
+     4 | X | 6
+    ---+---+---
+     7 | 8 | 9
+
+Mason (O), choose a position: 1
+
+"""
+
