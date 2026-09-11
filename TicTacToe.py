863d2c513b3eb43584312d5b99210621f6110d77
Xavier Chitison <chitisonx@gmail.com>
2026-09-11 12:11:13 -0400

    Display the current Tic-Tac-Toe

--- TicTacToe.py
+++ TicTacToe.py
@@ -38,3 +38,12 @@ Mason (O), choose a position: 1
 
 """
 
+def display_board(board):
+    """Display the current Tic-Tac-Toe board."""
+    print()
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
+    print()
